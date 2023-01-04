103616c5a34ca534f432092775b20967a2757b19
jhn2762 <jhn2762@gmail.com>
2023-01-04 17:24:06 +0900

    side menu1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import "./App.css";
-import "./layout/footer.css";
-import Header from "./layout/header";
-import Footer from "./layout/footer";
+import './App.css';
+import './layout/footer.css';
+import Header from './layout/header';
+import Menu from './layout/menu';
+// import Footer from './layout/footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       {/* <Footer /> */}
+      <Menu />
     </div>
   );
 }
