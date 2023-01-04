7e3ce53a835f99bc3604004225d8ce9dc9cab36f
yu isaac <isaac93948001@gmail.com>
2023-01-05 08:53:14 +0900

    2023-01-05 08:53

--- src/layout/menu.js
+++ src/layout/menu.js
@@ -1,16 +1,54 @@
-import './menu.css';
+import "./menu.css";
 
 const Menu = () => {
   return (
     <>
       <menu>
-        <nav>
-          <ul>
-            <li>
-              <a href="#none">js</a>
-            </li>
-          </ul>
-        </nav>
+        <ul className="side-nav">
+          <li className="side-main">
+            <a href="#none">JavaScript</a>
+            <ul className="side-sub">
+              <li>
+                <a href="#none">구글차트</a>
+              </li>
+              <li>
+                <a href="#none">리차트</a>
+              </li>
+            </ul>
+          </li>
+          <li className="side-main">
+            <a href="#none">REACT</a>
+            <ul className="side-sub">
+              <li>
+                <a href="#none">히히</a>
+              </li>
+            </ul>
+          </li>
+          <li className="side-main">
+            <a href="#none">PHP</a>
+            <ul className="side-sub">
+              <li>
+                <a href="#none">히히</a>
+              </li>
+            </ul>
+          </li>
+          <li className="side-main">
+            <a href="#none">Python</a>
+            <ul className="side-sub">
+              <li>
+                <a href="#none">히히</a>
+              </li>
+            </ul>
+          </li>
+          <li className="side-main">
+            <a href="#none">Custom</a>
+            <ul className="side-sub">
+              <li>
+                <a href="#none">히히</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </menu>
     </>
   );
