d3190d2a8a8c20f03ae4bd1b808c1d57d4e8cfdf
yu isaac <isaac93948001@gmail.com>
2023-01-04 17:22:15 +0900

    2023-01-04 헤더 스타일

--- src/layout/header.js
+++ src/layout/header.js
@@ -9,85 +9,102 @@ const Header = () => {
             "url(https://docs.popbill.com/static/img/bg/header_bg.jpg)",
         }}
       >
-        <h1>
-          <a href="#none">
-            <img
-              className="main-logo"
-              src={process.env.PUBLIC_URL + "/img/main-logo.png"}
-              alt=""
-            />
-          </a>
-        </h1>
-        <ul className="nav">
-          <li className="main">
-            메인메뉴 1
-            <ul className="sub">
-              <li>
-                <a href="#none">서브메뉴 1</a>
-              </li>
-              <li>
-                <a href="#none">서브메뉴 2</a>
-              </li>
-              <li>
-                <a href="#none">서브메뉴 3</a>
-              </li>
-              <li>
-                <a href="#none">서브메뉴 4</a>
-              </li>
-            </ul>
-          </li>
-          <li className="main">
-            메인메뉴 2
-            <ul className="sub">
-              <li>
-                <a href="#none">서브메뉴 1</a>
-              </li>
-              <li>
-                <a href="#none">서브메뉴 2</a>
-              </li>
-              <li>
-                <a href="#none">서브메뉴 3</a>
-              </li>
-              <li>
-                <a href="#none">서브메뉴 4</a>
-              </li>
-            </ul>
-          </li>
-          <li className="main">
-            메인메뉴 3
-            <ul className="sub">
-              <li>
-                <a href="#none">서브메뉴 1</a>
-              </li>
-              <li>
-                <a href="#none">서브메뉴 2</a>
-              </li>
-              <li>
-                <a href="#none">서브메뉴 3</a>
-              </li>
-              <li>
-                <a href="#none">서브메뉴 4</a>
-              </li>
-            </ul>
-          </li>
-          <li className="main">
-            메인메뉴 4
-            <ul className="sub">
-              <li>
-                <a href="#none">서브메뉴 1</a>
-              </li>
-              <li>
-                <a href="#none">서브메뉴 2</a>
-              </li>
-              <li>
-                <a href="#none">서브메뉴 3</a>
-              </li>
-              <li>
-                <a href="#none">서브메뉴 4</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
+        <a href="#none">
+          <img
+            className="main-logo"
+            src={process.env.PUBLIC_URL + "/img/main-logo.png"}
+            alt=""
+          />
+        </a>
+        <div className="pcmenu">
+          <ul className="nav">
+            <li className="main">
+              <a href="#none">JavaScript</a>
+              <ul className="sub">
+                <li>
+                  <a href="#none">서브메뉴 1</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 2</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 3</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 4</a>
+                </li>
+              </ul>
+            </li>
+            <li className="main">
+              <a href="#none">REACT</a>
+              <ul className="sub">
+                <li>
+                  <a href="#none">서브메뉴 1</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 2</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 3</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 4</a>
+                </li>
+              </ul>
+            </li>
+            <li className="main">
+              <a href="#none">PHP</a>
+              <ul className="sub">
+                <li>
+                  <a href="#none">서브메뉴 1</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 2</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 3</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 4</a>
+                </li>
+              </ul>
+            </li>
+            <li className="main">
+              <a href="#none">Python</a>
+              <ul className="sub">
+                <li>
+                  <a href="#none">서브메뉴 1</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 2</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 3</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 4</a>
+                </li>
+              </ul>
+            </li>
+            <li className="main">
+              <a href="#none">Custom</a>
+              <ul className="sub">
+                <li>
+                  <a href="#none">서브메뉴 1</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 2</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 3</a>
+                </li>
+                <li>
+                  <a href="#none">서브메뉴 4</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </header>
     </>
   );
